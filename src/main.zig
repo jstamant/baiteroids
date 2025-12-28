7278dea909be1b81b7bdaff801a72e2854976d10
Justin St-Amant <jstamant24@gmail.com>
2025-12-28 05:47:34 -0400

    Add rudimentary asteroid

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,23 @@ const std = @import("std");
 const rl = @import("raylib");
 const rlgl = @import("raylib").gl;
 
+const screenWidth = 1280;
+const screenHeight = 720;
 const shipAcceleration = 0.05;
+const asteroidAcceleration = 0.01;
 const shipTurnRate = 2;
 
 pub fn main() anyerror!void {
-    const screenWidth = 1280;
-    const screenHeight = 720;
-
     rl.initWindow(screenWidth, screenHeight, "Baiteroids!");
     defer rl.closeWindow();
-
     rl.setTargetFPS(60);
 
     var pos: rl.Vector2 = .{ .x = screenWidth / 2, .y = screenHeight / 2 };
     var rot: f32 = 0;
     var vel: rl.Vector2 = .{ .x = 0.2, .y = 0 };
+
+    var apos: rl.Vector2 = .{ .x = 0, .y = 0 };
+    var avel: rl.Vector2 = .{ .x = 0.2, .y = 0 };
 
     while (!rl.windowShouldClose()) {
         // Input
@@ -25,7 +27,10 @@ pub fn main() anyerror!void {
         if (rl.isKeyDown(.right)) rot += shipTurnRate;
 
         // Update
+        avel = updateAsteroidVelocity(avel, apos, pos);
+        avel = avel.clampValue(0.0, 2.0);
         pos = pos.add(vel);
+        apos = apos.add(avel);
 
         // Draw
         rl.beginDrawing();
@@ -36,6 +41,7 @@ pub fn main() anyerror!void {
         rl.drawText("Congrats! You created your first window!", 0, 0, 20, .light_gray);
 
         drawShip(pos, rot);
+        drawAsteroid(apos);
     }
 }
 
@@ -56,9 +62,23 @@ fn drawShip(pos: rl.Vector2, rot: f32) void {
     rl.drawCircleLines(0, 0, 10, .white);
 }
 
+fn drawAsteroid(pos: rl.Vector2) void {
+    const x = pos.x;
+    const y = pos.y;
+    rlgl.rlPushMatrix();
+    defer rlgl.rlPopMatrix();
+    rlgl.rlTranslatef(x, y, 0);
+    rl.drawCircleLines(0, 0, 40, .white);
+}
+
 fn updateShipVelocity(v: rl.Vector2, r: f32) rl.Vector2 {
     var moment: rl.Vector2 = .{ .x = shipAcceleration, .y = 0 };
     return v.add(moment.rotate(degreesToRadians(r)));
+}
+
+fn updateAsteroidVelocity(v: rl.Vector2, asteroid: rl.Vector2, ship: rl.Vector2) rl.Vector2 {
+    const moment: rl.Vector2 = ship.subtract(asteroid).normalize().scale(asteroidAcceleration);
+    return v.add(moment);
 }
 
 fn degreesToRadians(degrees: f32) f32 {
